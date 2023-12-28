6a7162106487be082725e77496c8b14e871fe78b
lex-sucena <alex.everythingelse@gmail.com>
2023-12-28 11:04:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+const submitForm = document.getElementById("submitForm");
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const salario = Number(document.getElementById("salario").value);
+  const vtDia = Number(document.getElementById("vtDia").value);
+  const vtTotal = document.getElementById("vtTotal");
+  const descUser = document.getElementById("descUser");
+  const vtEmpresa = document.getElementById("vtEmpresa");
+
+  const descMax = Number((salario / 100) * 6).toFixed(2);
+  if (descMax < vtDia * 21) {
+    vtTotal.innerHTML = `${vtDia * 21}`;
+    descUser.innerHTML = `${descMax}`;
+    vtEmpresa.innerHTML = `${vtDia * 21 - descMax}`;
+  } else if (descMax > vtDia * 21) {
+    vtTotal.innerHTML = `${vtDia * 21}`;
+    descUser.innerHTML = `${vtDia * 21}`;
+    vtEmpresa.innerHTML = `0`;
+  } 
+});
